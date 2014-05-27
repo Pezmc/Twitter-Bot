2d3f9e34ef6d5caf6a1492c22b83056d214f621b
Pez Cuckow <email@pezcuckow.com>
2014-05-27 17:17:07 +0100

    Package index with coverage testing flag

---
+++ index.js
@@ -0,0 +1,4 @@
+// To enablecoverage testing set the environment variable TWITTER_BOT_COV = 1 
+var libPath = process.env['TWITTER_BOT_COV'] ? '../lib-cov' : '../lib';
+
+module.exports = require(libPath + '/twitter-bot');
